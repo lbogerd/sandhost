ee47d5b20c086c7d0db3ae17b748209a7827da75
Lucas Bogerd <lucas@footsteps.nl>
2026-06-06 00:41:08 +0200

    feat: add tanstack router

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite"
 import react, { reactCompilerPreset } from "@vitejs/plugin-react"
 import babel from "@rolldown/plugin-babel"
 import tailwindcss from "@tailwindcss/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		tailwindcss(),
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
 	server: {
 		port: 3000,
 	},
